f6e899b980d7edef3a7f6df5481ce50581abfc7c
shantanu400 <ashunayan400@gmail.com>
2024-08-08 13:42:20 +0530

    updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function promise3(){  //for resolving promise3
             let req=fetch('https://dummyjson.com/todos')  //fetching API 
         .then(data=>data.json())
         .then(data=>{
-            console.log(data)
+           
             let promise3div=document.createElement('div')
             promise3div.textContent=JSON.stringify(data)   // converting o JSON string
             document.body.appendChild(promise3div);
@@ -40,7 +40,7 @@ function promise2(){
             let req=fetch('https://dummyjson.com/products')  //fetching API 
         .then(data=>data.json())
         .then(data=>{
-            console.log(data)
+         
             let promise2div=document.createElement('div')
             promise2div.textContent=JSON.stringify(data)   // converting o JSON string
             document.body.appendChild(promise2div);
@@ -63,7 +63,7 @@ function promise1(){
             let req=fetch('https://dummyjson.com/posts')  //fetching API 
         .then(data=>data.json())
         .then(data=>{
-            console.log(data)
+          
             let promise1div=document.createElement('div')
             promise1div.textContent=JSON.stringify(data)   // converting o JSON string
             document.body.appendChild(promise1div);
